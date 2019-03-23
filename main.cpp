71b1d9f4363c426067988cdf8af7a77050d5f1a3
Evan Crabtree <ecrab21@gmail.com>
2019-03-23 20:54:26 +0000

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, const char * argv[]) {
+    int inverse(int, int);
+    int e, m, result;
+    
+    cout << "Enter value for e: ";
+    cin >> e;
+    cout << "Enter value for the totient: ";
+    cin >> m;
+    
+    result = inverse(e, m);
+    
+    (result == -1) ? cout << "Invalid" : cout << to_string(result);
+    cout << "\n";
+    
+    cin.get();
+    cin.get();
+    return 0;
+}
+
+int inverse(int a, int m) {
+    a = a % m;
+    for (int x(1); x < m; x++) {
+        if ((a * x) % m == 1) {
+            return x;
+        }
+    }
+    return -1;
+}
